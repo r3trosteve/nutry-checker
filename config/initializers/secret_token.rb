21f9c7ac0efdfcf83ae15ed9cbbc60a04eb50406
steve <stevebschofield@gmail.com>
2013-01-14 10:50:53 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NutryChecker::Application.config.secret_token = '338e849a082d49990eae41e794080fbed285f8906bfa361601d69a6c65acb6f8bb4b35d98088766adb2d7666fd5eccdd5a3b98d3f5b312a2de2e3813998aed48'
